0ea1343af4aaf0c55e427df5c2782b0896e0f3ec
Matthew <matthaldeman95@gmail.com>
2018-08-14 17:39:33 -0700

    Break reminders into separate Python file, improve logic

--- haldibot.py
+++ haldibot.py
@@ -11,15 +11,18 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 from snips_nlu import SnipsNLUEngine, load_resources
 from snips_nlu.default_configs import CONFIG_EN
+from dateutil import parser
 import platform
+from reminder import set_reminder
 
 print("Loading NLU engine...")
 # Load up Snips engine
 load_resources(u"en")
 engine = SnipsNLUEngine(config=CONFIG_EN)
-with io.open('dataset.json') as f:
-    dataset = json.load(f)
-engine.fit(dataset)
+#with io.open('data/dataset.json') as f:
+#    dataset = json.load(f)
+#engine.fit(dataset)
+engine = SnipsNLUEngine.from_path("data/engine")
 
 client = Bot(description="haldibot.", command_prefix="-", pm_help = False)
 
@@ -27,13 +30,16 @@ print("haldibot lives!")
 
 @client.event
 async def on_ready():
-	return await client.change_presence(game=discord.Game(name='L E A R N I N G'))
+	return await client.change_presence(game=discord.Game(name='with your heart'))
+
+@client.command()
+async def fuckyou(*args):
+	response = "no fuck you"
+	await client.say(response)
 
 @client.command()
 async def echo(*args):
-
 	response = " ".join(args)
-	response = "no"
 	await client.say(response)
 
 @client.command()
@@ -41,28 +47,41 @@ async def add(left: int, right: int):
 	await client.say(left + right)
 
 @client.command()
-async def remind(*args):
+async def subtract(left: int, right: int):
+	await client.say(left - right)
+
+@client.command(pass_context=True)
+async def remind(ctx, *args):
 
 	request = "remind " + " ".join(args)
 	result = engine.parse(request)
+	intent = None
+	dt_string = None
+	if not result['slots']:
+		return await client.say("I can't **** understand **** your accent ****")
 	for s in result['slots']:
 		if s['slotName'] == 'intent':
 			intent = s['value']['value']
 		if s['slotName'] == 'time':
 			dt_string = s['value']['value']
-	utc_offset = dt_string.rsplit(':', 1)
-	dt_string = ''.join(utc_offset)
-	format_string = "%Y-%m-%d %H:%M:%S %z"
-	dt = datetime.datetime.strptime(dt_string, format_string)
+	if not intent:
+		return await client.say("It isn't clear to me what to remind you about.")
+	if not dt_string:
+		return await client.say("I know what you want to be reminded of, but not what time to remind you.")
+	dt = parser.parse(dt_string)
+	user_id = ctx.message.author.id
+	channel_id = ctx.message.channel.id
+	set_reminder(intent, dt, user_id, channel_id)
+	
 	output_string_format = "%I:%M %p on %a, %b %d"
 	output_time = datetime.datetime.strftime(dt, output_string_format)
 
-	output_string = "I will remind me you to `{}` at `{}` (but not really, I haven't yet learned how)".format(intent, output_time)
+	output_string = "<@{}>, I will remind me you to `{}` at `{}`".format(user_id, intent, output_time)
 	return await client.say(output_string)
 
 @client.command(pass_context=True)
 async def hello(ctx):
-	message = "Hello, {}!  Have a nice day.".format(str(ctx.message.author).split('#')[0])
+	message = "Hello, <@{}>!  Have a nice day.".format(str(ctx.message.author.id))
 	return await client.say(message)
 
 @client.command()
