2b23ee74cf4cd14cd16fbc9ce507489fcd89cffb
Matthew <matthaldeman95@gmail.com>
2019-06-26 17:05:03 -0700

    remove unnecessary variable

--- discord_commands.py
+++ discord_commands.py
@@ -1,4 +1,4 @@
-async def get_message(bot_instance, context):
+async def get_message(context):
     message = context.message
     channel = context.channel
     async for elem in channel.history(limit=1, before=message):
